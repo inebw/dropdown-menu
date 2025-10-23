79d3eb431bb062de869ea966bccaee45a1bbe3c8
inebw <punit965@gmail.com>
2025-10-23 20:43:33 +0530

    Gif searcher

--- src/index.js
+++ src/index.js
@@ -1,21 +1,44 @@
 import "./styles.css";
+import loadingImage from "./loading.gif";
+import whatYouNeed from "./whatyouneed.gif"
 
 const img = document.querySelector("img");
-const params = new URLSearchParams();
-params.append("api_key", "UG2Rfo0dLmELewpayLFF56JS6bhSV3Pz");
-params.append("s", "shiba");
-const fetchURL =
-  `https://api.giphy.com/v1/gifs/translate?${params}`;
+const shibaButton = document.querySelector(".shiba");
+const searchButton = document.querySelector(".search");
+const searchInput = document.getElementById("search");
+const form = document.querySelector('form');
 
-fetch(fetchURL)
-  .then((response) => {
-    if (!response.ok) throw new Error('what the heck')
-    return response.json()
-  })
-  .then((response) => {
-    img.src = response.data.images.original.url;
-  })
-  .catch((error) => {
-    img.alt = 'some error happened'
-    console.log(error)
-  })
+img.src = whatYouNeed;
+
+function addImage(searchGIF = "shibainu") {
+  img.src = loadingImage;
+  const params = new URLSearchParams();
+  params.append("api_key", "UG2Rfo0dLmELewpayLFF56JS6bhSV3Pz");
+  params.append("s", searchGIF);
+  const fetchURL = `https://api.giphy.com/v1/gifs/translate?${params}`;
+  console.log(fetchURL, searchGIF);
+  fetch(fetchURL)
+    .then((response) => {
+      if (!response.ok) throw new Error("what the heck");
+      return response.json();
+    })
+    .then((response) => {
+      img.src = response.data.images.original.url;
+    })
+    .catch((error) => {
+      img.alt = "some error happened";
+      throw error;
+    });
+}
+
+shibaButton.addEventListener("click", () => {
+  addImage();
+});
+searchButton.addEventListener("click", () => {
+  addImage(searchInput.value);
+});
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  addImage(searchInput.value);
+})
